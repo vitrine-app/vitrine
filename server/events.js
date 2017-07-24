44ed5d09a93861ab9ef452189e8f12518647e3e8
Paul Roman <paul.roman@epitech.eu>
2017-07-24 20:03:31 +0200

    wrapper variable made global

--- server/events.js
+++ server/events.js
@@ -1,9 +1,10 @@
 const IgdbWrapper = require('./api/IgdbWrapper');
 
+let igdbWrapper = new IgdbWrapper();
+
 module.exports = events = {
 	'client.get-game': (event, args) => {
-		let wrapper = new IgdbWrapper();
-		wrapper.getGame(args, function(doc) {
+		igdbWrapper.getGame(args, function(doc) {
 			event.sender.send('server.send-game', doc);
 		});
 	}
