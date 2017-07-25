f09df7746328e1296a9440b8deb734c732846f33
Paul Roman <paul.roman@epitech.eu>
2017-07-26 01:23:44 +0200

    appManifests crawler

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const Vitrine = require('./server/Vitrine');
 const events = require('./server/events');
 
+/*
 vitrineApp = new Vitrine();
 vitrineApp.registerEvents(events);
 
 vitrineApp.run(true);
+*/
+
+require('./server/steam');
---
+++ server/steam.js
@@ -0,0 +1,20 @@
+const glob = require('glob');
+const path = require('path');
+const steamConfig = require('../config/steam.json');
+
+steamConfig.gamesFolders.forEach(function(folder) {
+	let gameFolder = '';
+
+	if (folder.startsWith('~')) {
+		gameFolder = path.join(steamConfig.installFolder, folder.substr(1), 'appmanifest_*.acf');
+	}
+	else
+		gameFolder = path.join(folder, 'appmanifest_*.acf');
+	glob(gameFolder, globCallback);
+});
+
+function globCallback(err, files) {
+	files.forEach(function(appManifest) {
+		console.log(appManifest);
+	});
+}
