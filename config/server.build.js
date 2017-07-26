47e90bb11be4d8b666978f8311370acde3301528
Paul Roman <paul.roman@epitech.eu>
2017-07-26 15:22:16 +0200

    Build config files

---
+++ config/server.build.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: './assets/ts/server/main.ts',
+		output: {
+	filename: 'main.js'
+},
+	node: {
+		__dirname: false,
+			__filename: false
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json']
+	},
+	target: "electron-main",
+		module: {
+	loaders: [
+		{
+			test: /\.ts?$/,
+			loader: 'awesome-typescript-loader'
+		}
+	]
+}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,4 @@
-module.exports = [
-	/* Server build process */
-	{
-		entry: './assets/ts/server/main.ts',
-		output: {
-			filename: 'main.js'
-		},
-		node: {
-			__dirname: false,
-			__filename: false
-		},
-		resolve: {
-			extensions: ['.ts', '.js', '.json']
-		},
-		target: "electron-main",
-		module: {
-			loaders: [
-				{
-					test: /\.ts?$/,
-					loader: 'awesome-typescript-loader'
-				}
-			]
-		}
-	}
-];
+const serverBuild = require('./config/server.build');
+const clientBuild = require('./config/client.build');
+
+module.exports = [serverBuild, clientBuild];
