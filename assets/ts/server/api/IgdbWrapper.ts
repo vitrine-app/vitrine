e74e1f42eb545fb72202a28c0e13e672464448f3
Paul Roman <paul.roman@epitech.eu>
2017-08-04 13:35:25 +0200

    Igdb algorithm reworked with Levenshtein

--- assets/ts/server/api/IgdbWrapper.ts
+++ assets/ts/server/api/IgdbWrapper.ts
@@ -1,65 +1,80 @@
 import * as igdb from 'igdb-api-node';
+import * as getClosest from 'get-closest';
+
+import {levenshteinDistanceCmp, nameArray} from '../helpers';
 
 class IgdbWrapper {
 	private apiKey: string;
 	private client: any;
 	private operating: boolean;
-	private currentCallback: any;
-	private currentGame: any;
+	private levenshteinRefiner: number;
+	private callback: any;
+	private game: any;
 
 	constructor() {
 		this.apiKey = 'XBbCSfnCremsh2OsjrJlRE83AIbmp1ZMAbtjsn7puoI7G57gpl';
 		this.client = igdb.default(this.apiKey);
 		this.operating = false;
+		this.levenshteinRefiner = 5;
 
-		this.currentGame = null;
-		this.currentCallback = null;
+		this.game = null;
+		this.callback = null;
 	}
 
 	public getGame(name, callback) {
 		this.operating = true;
+		this.client.games({
+			limit: this.levenshteinRefiner,
+			search: name
+		}, ['name']).then((response) => {
+			let gamesNames: string[] = nameArray(response.body);
+			let gameId: number = getClosest.custom(name, gamesNames, levenshteinDistanceCmp);
 
-		this.findGameByName(name, (error, game) => {
-			if (error && !game) {
-				console.error(error);
-				callback(name + ' not found.', null);
-			}
+			this.findGameById(response.body[gameId].id, (error, game) => {
+				if (error && !game) {
+					console.error(error);
+					callback(name + ' not found.', null);
+				}
 
-			this.currentGame = game;
-			this.currentCallback = callback;
+				this.game = game;
+				this.callback = callback;
 
-			if (this.currentGame) {
-				this.basicFormatting();
-				this.findCompanyById(game.developers, this.addDeveloperCallback.bind(this));
-			}
+				if (this.game) {
+					this.basicFormatting();
+					this.findCompanyById(game.developers, this.addDeveloperCallback.bind(this));
+				}
+			});
+		}).catch((error) => {
+			if (error)
+				this.callback(error, null);
 		});
 	}
 
 	private basicFormatting() {
-		if (this.currentGame.total_rating) {
-			let rating = this.currentGame.total_rating;
-			this.currentGame.rating = Math.round(rating);
-			delete this.currentGame['total_rating'];
+		if (this.game.total_rating) {
+			let rating = this.game.total_rating;
+			this.game.rating = Math.round(rating);
+			delete this.game['total_rating'];
 		}
-		if (this.currentGame.cover)
-			this.currentGame.cover = 'https:' + this.currentGame.cover.url.replace('t_thumb', 't_cover_big_2x');
+		if (this.game.cover)
+			this.game.cover = 'https:' + this.game.cover.url.replace('t_thumb', 't_cover_big_2x');
 		else /* TODO: Change default image */
-			this.currentGame.cover = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/nocover_qhhlj6.jpg';
-		if (this.currentGame.screenshots) {
-			this.currentGame.screenshots.forEach((element, key) => {
-				this.currentGame.screenshots[key] = 'https:' + element.url.replace('t_thumb', 't_screenshot_med');
+			this.game.cover = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/nocover_qhhlj6.jpg';
+		if (this.game.screenshots) {
+			this.game.screenshots.forEach((element, key) => {
+				this.game.screenshots[key] = 'https:' + element.url.replace('t_thumb', 't_screenshot_med');
 			});
 		}
 		else
-			this.currentGame.screenshots = [];
+			this.game.screenshots = [];
 	}
 
-	private findGameByName(name, callback) {
+	private findGameById(id, callback) {
 		this.client.games({
-			limit: 1,
-			filters: {
+			ids: [id]
+			/*filters: {
 				'name-eq': name
-			}
+			}*/
 		}, [
 			'name',
 			'summary',
@@ -72,7 +87,8 @@ class IgdbWrapper {
 			'screenshots',
 			'cover'
 		]).then((response) => {
-			callback(null, response.body[0]);
+			let firstGame = response.body[0];
+			callback(null, firstGame);
 		}).catch((error) => {
 			callback(error, null);
 		});
@@ -119,22 +135,22 @@ class IgdbWrapper {
 	}
 
 	private addDeveloperCallback(developer) {
-		this.currentGame.developers = developer.name;
+		this.game.developers = developer.name;
 
-		this.findCompanyById(this.currentGame.publishers, this.addPublisherCallback.bind(this));
+		this.findCompanyById(this.game.publishers, this.addPublisherCallback.bind(this));
 	}
 
 	private addPublisherCallback(publisher) {
-		this.currentGame.publishers = publisher.name;
+		this.game.publishers = publisher.name;
 
-		this.findSeriesById(this.currentGame.collection, this.addSeriesCallback.bind(this));
+		this.findSeriesById(this.game.collection, this.addSeriesCallback.bind(this));
 	}
 
 	private addSeriesCallback(series) {
-		delete this.currentGame['collection'];
-		this.currentGame.series = series.name;
+		delete this.game['collection'];
+		this.game.series = series.name;
 
-		this.findGenreById(this.currentGame.genres, this.addGenresCallback.bind(this));
+		this.findGenreById(this.game.genres, this.addGenresCallback.bind(this));
 	}
 
 	private addGenresCallback(genres) {
@@ -142,11 +158,9 @@ class IgdbWrapper {
 		genres.forEach((genre) => {
 			genresArray.push(genre.name);
 		});
-		this.currentGame.genres = genresArray;
+		this.game.genres = genresArray;
 
-		this.currentCallback(null, this.currentGame);
-		delete this.currentGame;
-		delete this.currentCallback;
+		this.callback(null, this.game)
 		this.operating = false;
 	}
 }
--- assets/ts/server/helpers.ts
+++ assets/ts/server/helpers.ts
@@ -1,8 +1,9 @@
 import * as http from 'http';
 import * as https from 'https';
 import * as fs from 'fs';
-import * as uuid from 'uuid/v5';
 import * as path from 'path';
+import * as uuid from 'uuid/v5';
+import * as Levenshtein from 'levenshtein';
 
 let isEnvProd: boolean = (JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')).toString()).env) ? (true) : (false);
 
@@ -28,4 +29,16 @@ export function getGamesFolder() {
 
 export function getScriptsFolder() {
 	return path.resolve(__dirname, (isEnvProd) ? ('../../scripts') : ('../scripts'));
+}
+
+export function nameArray(array) {
+	let namesArray: string[] = [];
+
+	for (let i in array)
+		namesArray.push(array[i].name)
+	return namesArray;
+}
+
+export function levenshteinDistanceCmp(node, baseItem) {
+	return new Levenshtein(node, baseItem).distance;
 }
