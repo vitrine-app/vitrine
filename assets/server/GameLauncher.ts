a2642ab0b18b7f3927195cb083597200800e8f51
Paul Roman <paul.roman@epitech.eu>
2018-03-22 16:54:23 +0000

    GameLauncher documented with logs

--- assets/server/GameLauncher.ts
+++ assets/server/GameLauncher.ts
@@ -4,6 +4,7 @@ import { GameSource } from '../models/PotentialGame';
 import { PlayableGame } from '../models/PlayableGame';
 import { launchGame as nativeLaunchGame, GameLauncherOptions } from '../../scripts/gameLauncher.node';
 import { watchRegKey } from '../../scripts/regWatcher.node'
+import { logger } from './Logger';
 
 class GameLauncher {
 	private game: PlayableGame;
@@ -19,6 +20,8 @@ class GameLauncher {
 	}
 
 	private launchStandardGame() {
+		logger.info('GameLauncher', 'Launching non-Steam game.');
+
 		let [ executable, args ]: string[] = this.game.commandLine;
 		let launcherOptions: GameLauncherOptions = {
 			program: executable,
@@ -27,11 +30,14 @@ class GameLauncher {
 		if (args)
 			launcherOptions.args = args;
 
+		logger.info('GameLauncher', `Launching game ${executable} with native module.`);
 		nativeLaunchGame(launcherOptions, (error: string, secondsPlayed: number) => {
 			if (error)
 				this.callback(new Error(error), null);
-			else
+			else {
+				logger.info('GameLauncher', `Game terminated (played during ${secondsPlayed} seconds).`);
 				this.callback(null, secondsPlayed);
+			}
 		});
 	}
 
@@ -41,24 +47,30 @@ class GameLauncher {
 			return;
 		}
 
+		logger.info('GameLauncher', 'Launching Steam game.');
+
 		let regNest: string = 'HKEY_CURRENT_USER';
 		let regKey: string = `Software\\Valve\\Steam\\Apps\\${this.game.details.steamId}`;
-
+		logger.info('GameLauncher', `Watching Steam registry key for game begin (${this.game.details.steamId}).`);
 		watchRegKey(regNest, regKey, (error: string) => {
 			if (error) {
 				this.callback(new Error(error), null);
 				return;
 			}
 
+			logger.info('GameLauncher', `Watching Steam registry key for game end (${this.game.details.steamId}).`);
 			watchRegKey(regNest, regKey, (error: string, secondsPlayed: number) => {
 				if (error)
 					this.callback(new Error(error), null);
-				else
+				else {
+					logger.info('GameLauncher', `Game terminated (played during ${secondsPlayed} seconds).`);
 					this.callback(null, secondsPlayed);
+				}
 			});
 		});
 
 		let [ program, args ]: string[] = this.game.commandLine;
+		logger.info('GameLauncher', `Launching Steam game (${this.game.details.steamId}}) with native module.`);
 		nativeLaunchGame({ program, args });
 	}
 }
