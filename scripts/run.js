72564d8ed033dffb10e694ee8b0162ac516e8869
Paul Roman <paul.roman@epitech.eu>
2018-04-03 22:47:22 +0100

    Update build modules script
    Use generic JS script to execute Bash scripts
    Call build_modules.sh from run.js

--- scripts/run.js
+++ scripts/run.js
@@ -2,11 +2,14 @@
  * Allows the Bash script to be executed, regardless of the platform used.
  */
 
+if (!process.argv[2])
+	throw new Error('Script name missing.');
+
 if (process.platform === 'win32') {
 	const { cd, exec } = require('shelljs');
-	cd('scripts').exec('build_modules.sh');
+	cd('scripts').exec(process.argv[2]);
 }
 else {
 	const { exec } = require('child_process');
-	exec('sh scripts/build_modules.sh');
+	exec(`sh scripts/${process.argv[2]}`);
 }
