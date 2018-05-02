2cb69985bd4624ce05eae3b1ce71ae0f345d9e84
Paul Roman <paul.roman@epitech.eu>
2018-05-02 02:42:12 +0100

    Use transitions for PotentialGamesAddModal
    Use standard React component instead of StatelessComponent

--- sources/client/app/components/PotentialGamesAddModal.tsx
+++ sources/client/app/components/PotentialGamesAddModal.tsx
@@ -1,7 +1,7 @@
 import { css, StyleSheet } from 'aphrodite';
 import { margin } from 'css-verbose';
 import * as React from 'react';
-import { Grid, Modal } from 'semantic-ui-react';
+import { Grid, Modal, Transition } from 'semantic-ui-react';
 
 import { GamesCollection } from '../../../models/GamesCollection';
 import { PotentialGame } from '../../../models/PotentialGame';
@@ -9,6 +9,7 @@ import { localizer } from '../Localizer';
 import { BlurPicture } from './BlurPicture';
 
 import { faPlusCircle } from '@fortawesome/fontawesome-free-solid';
+import { VitrineComponent } from './VitrineComponent';
 
 interface Props {
 	potentialGames: GamesCollection<PotentialGame>;
@@ -18,42 +19,74 @@ interface Props {
 	closePotentialGamesAddModal: () => void;
 }
 
-export const PotentialGamesAddModal: React.StatelessComponent<Props> = (props: Props) => {
-	const gameCoverClickHandler = (potentialGame: PotentialGame) => {
-		props.setPotentialGameToAdd(potentialGame);
-		props.closePotentialGamesAddModal();
-		props.openGameAddModal();
-	};
+interface State {
+	transitionVisible: boolean;
+}
 
-	return (
-		<Modal
-			open={props.visible}
-			onClose={props.closePotentialGamesAddModal}
-			className={css(styles.modal)}
-		>
-			<Modal.Header>{localizer.f('addGames')}</Modal.Header>
-			<Modal.Content>
-				<Grid>
-					{props.potentialGames.map((potentialGame: PotentialGame, index: number) =>
-						<Grid.Column width={3} key={index}>
-							<div className={css(styles.coverWrapper)}>
-								<BlurPicture
-									faIcon={faPlusCircle}
-									fontSize={55}
-									background={potentialGame.details.cover}
-									clickHandler={gameCoverClickHandler.bind(null, potentialGame)}
-								/>
-							</div>
-							<p className={css(styles.potentialGameName)}>
-								{potentialGame.name}
-							</p>
-						</Grid.Column>
-					)}
-				</Grid>
-			</Modal.Content>
-		</Modal>
-	);
-};
+export class PotentialGamesAddModal extends VitrineComponent<Props, State> {
+	public constructor(props: Props) {
+		super(props);
+
+		this.state = {
+			transitionVisible: true
+		};
+
+		this.gameCoverClick = this.gameCoverClick.bind(this);
+		this.animateModal = this.animateModal.bind(this);
+	}
+
+	private gameCoverClick(potentialGame: PotentialGame) {
+		this.props.setPotentialGameToAdd(potentialGame);
+		this.props.closePotentialGamesAddModal();
+		this.props.openGameAddModal();
+	}
+
+	private animateModal(startingAnimation: boolean) {
+		if (startingAnimation === this.props.visible)
+			this.setState({
+				transitionVisible: this.props.visible
+			});
+	}
+
+	public render(): JSX.Element {
+		return (
+			<Transition
+				animation={'fade down'}
+				duration={400}
+				onStart={this.animateModal.bind(this, true)}
+				onComplete={this.animateModal.bind(this, false)}
+				visible={this.props.visible}
+			>
+				<Modal
+					open={this.state.transitionVisible}
+					onClose={this.props.closePotentialGamesAddModal}
+					className={css(styles.modal)}
+				>
+					<Modal.Header>{localizer.f('addGames')}</Modal.Header>
+					<Modal.Content>
+						<Grid>
+							{this.props.potentialGames.map((potentialGame: PotentialGame, index: number) =>
+								<Grid.Column width={3} key={index}>
+									<div className={css(styles.coverWrapper)}>
+										<BlurPicture
+											faIcon={faPlusCircle}
+											fontSize={55}
+											background={potentialGame.details.cover}
+											clickHandler={this.gameCoverClick.bind(null, potentialGame)}
+										/>
+									</div>
+									<p className={css(styles.potentialGameName)}>
+										{potentialGame.name}
+									</p>
+								</Grid.Column>
+							)}
+						</Grid>
+					</Modal.Content>
+				</Modal>
+			</Transition>
+		);
+	}
+}
 
 const styles: React.CSSProperties & any = StyleSheet.create({
 	modal: {
