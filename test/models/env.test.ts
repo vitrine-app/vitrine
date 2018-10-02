7d87db45aa21aca946571cd8ab869280a11c17c2
Paul Roman <paul.roman@epitech.eu>
2018-10-02 21:16:10 +0200

    refactor: use spaces as indentation for tests

--- test/models/env.test.ts
+++ test/models/env.test.ts
@@ -1,28 +1,28 @@
 import { randomHashedString, uuidV5 } from '../../sources/models/env';
 
 describe('helpers functions', () => {
-	describe('uuidV5()', () => {
-		let uuid: string;
-		before(() => {
-			uuid = uuidV5('dummy');
-		});
-		it('Return a 36 characters length string', () => {
-			uuid.should.have.length(36);
-		});
-		it('Have 5 hyphen-separated parts', () => {
-			uuid.split('-').should.have.length(5);
-		});
-	});
+  describe('uuidV5()', () => {
+    let uuid: string;
+    before(() => {
+      uuid = uuidV5('dummy');
+    });
+    it('Return a 36 characters length string', () => {
+      uuid.should.have.length(36);
+    });
+    it('Have 5 hyphen-separated parts', () => {
+      uuid.split('-').should.have.length(5);
+    });
+  });
 
-	describe('randomHashedString()', () => {
-		let hashedString: string;
-		let length: number;
-		before(() => {
-			length = 12;
-			hashedString = randomHashedString(length);
-		});
-		it('Return a hashed string of the corresponding length', () => {
-			hashedString.should.have.length(length);
-		});
-	});
+  describe('randomHashedString()', () => {
+    let hashedString: string;
+    let length: number;
+    before(() => {
+      length = 12;
+      hashedString = randomHashedString(length);
+    });
+    it('Return a hashed string of the corresponding length', () => {
+      hashedString.should.have.length(length);
+    });
+  });
 });
